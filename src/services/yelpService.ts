2f507f11a41fc6a0027cc9c0213f2dc967916911
Damiquel Williams <damiquel@Damiquels-MacBook-Pro.local>
2022-08-25 16:14:55 -0400

    yeld service

--- src/services/yelpService.ts
+++ src/services/yelpService.ts
@@ -1,17 +1,9 @@
 import axios from "axios";
-import MultipleYelpResponse from "../models/MultipleYelpResponse";
-
-const key: string = process.env.REACT_APP_YELP_API_KEY || "";
-
-export const getBusinessesByLocation = async (
-  location: string
-): Promise<MultipleYelpResponse> => {
+const baseURL: string = "http://localhost:5001";
+export const searchYelp = async (location: string): Promise<any> => {
   return (
-    await axios.get("https://api.yelp.com/v3/businesses/search", {
+    await axios.get(baseURL, {
       params: { location },
-      headers: {
-        Authorization: `Bearer ${key}`,
-      },
     })
   ).data;
 };
